80f5a17632b5fe42ba689009a113dacd253fa6e7
vivek77066 <153838967+vivek77066@users.noreply.github.com>
2025-06-20 20:12:25 +0530

    complete project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <>
+      <Card name="vivek" village="sokhda"/>
+      <Card name="jagdish" village="madhgam "/>
+    </>
+  );
+}
+
+export default App;
